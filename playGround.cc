c3c4eae135a0d4c716d599402df98a2ba80fc643
= <fbl773@mail.usask.ca>
2017-11-01 23:27:07 -0600

    Now tracking node colour changes in the array of ledNodes.

--- playGround.cc
+++ playGround.cc
@@ -9,17 +9,6 @@
 
 using namespace std ; 
 
-/*
-//Structs
-struct ledNode{
-
-//Colours
-int r, g, b;
-
-
-};
-
-*/
 //For test purposes!
 void printArray(char charArr[]){
 	int cap = getArrLen(charArr);
@@ -31,125 +20,6 @@ void printArray(char charArr[]){
 }
 
 
-
-
-//ledNode tools
-/*
-* setNodeColour(r,g,b,n)
-* - sets the colour of node n to the int r g b value paramaters.
-* 
-* preconditions:
-* - n is not NULL
-*
-* postconditions:
-* - the r g b attributes of n are changed
-* return: nothing
-*/
-// void setNodeColour(int r, int g, int b, ledNode* node){
-// 	
-// 	if(node == NULL){
-// 		cout<<"node was NULL, exiting"<<endl;
-// 		return;
-// 	}
-// 
-// 	node->r = r;
-// 	node->g = g;
-// 	node->b = b;
-// 	return;
-// }
-
-/*
-* newLedNode()
-* - the struct equivilent of a constructor
-*
-* precond: none
-* postcond: new node declared and instantiated.
-* return: a pointer to the new node.
-*/
-// ledNode* newLedNode(){
-// 	ledNode* node = new ledNode();
-// 	setNodeColour(30,195,195,node);	//Default node settings 60 + 195 = 255
-// 
-// 	return node;
-// }
-//  	
-// /*
-// * nodeStats(n)
-// * prints the stats of ledNode* n
-// * precond: n != NULL
-// * postcond: stats desplayed
-// * return: nothing 
-// */
-// void nodeStats(ledNode* node){	
-// 
-// 	cout<<"Colours were: "<<endl;
-// 	cout<<"Red: "<<node->r<<endl;
-// 	cout<<"Green: "<<node->g<<endl;
-// 	cout<<"Blue: " <<node->b<<endl;
-// 	return;
-// }
-// 
-// void setBlueSecond(ledNode* node, int addMe){
-// 	addMe += node->b;
-// 	int g = node->g;
-// 	
-// 		cout<<"about to make node blue value: "<<addMe<<endl;
-// 	
-// 	setNodeColour(30,g,addMe,node);
-// 	
-// 	return;
-// }
-// 
-// void setGreenMinute(ledNode* node, int addMe){
-// 	addMe += node->g;
-// 	
-// 	int b = node->b;
-// 	cout<<"about to make node green value: "<<addMe<<endl;
-// 	
-// 	setNodeColour(30,addMe,b,node);
-// 	
-// 	return;
-// 	}
-// 
-// void setAverageCross(ledNode* hrNode, ledNode*minNode){
-// 	
-// 	minNode->g = (hrNode->g + minNode->g) / 2;
-// 	minNode->b = (hrNode->b + minNode->b) / 2;
-// 	
-// 	return;
-// 	}
-// 	
-// ledNode* genNodeArray(tm* time){
-// 	
-// 	ledNode* nodeArr = new ledNode[12];
-// 	
-// 	for(int i = 0; i < 12; i++)
-// 		nodeArr[i] = *newLedNode();	
-// 
-// 	
-// 	int hrIndex = getHourIndex(time);
-// 	int minIndex = getMinuteIndex(time);
-// 	
-// 	//Assign Values
-// 	
-// 	if(hrIndex==minIndex){
-// 		
-// 		setAverageCross(&nodeArr[hrIndex],&nodeArr[minIndex]);
-// 		//setNodeColour(time->tm_hour,time->tm_min,time->tm_sec,&nodeArr[minIndex]);
-// 	}
-// 	else{
-// 		// setNodeColour(time->tm_hour,time->tm_min,time->tm_sec,&nodeArr[hrIndex]);
-// // 		setNodeColour(0,128,0,&nodeArr[minIndex]);
-// 		
-// 		setGreenMinute(&nodeArr[hrIndex], time->tm_min);
-// 		setBlueSecond(&nodeArr[minIndex], time->tm_sec);
-// 	}
-// 		
-// 	nodeStats(&nodeArr[hrIndex]);
-// 	nodeStats(&nodeArr[minIndex]);
-// 	return nodeArr;
-// }
-	
 
 int main() {
 	cout<<"Begin Watch playground"<<endl;	
@@ -195,12 +65,12 @@ int main() {
 	ledNode* ledArr = genNodeArray(timeII);
 	
 	//possible new way to check node stats. work on later!
-	/*
+	
 	for(int i =0; i < 12; i++){
-		int nodeRGB = nodeStats(*ledArr[i]);
-		cout<<" node: "<<i<<" r="<<nodeRGB[0]<<" g="<<nodeRGB[1]<<" b="<<nodeRBG[2]<<endl;
+		int* nodeRGB = nodeStats(&ledArr[i]);
+		cout<<" node: "<<i<<" r="<<nodeRGB[0]<<" g="<<nodeRGB[1]<<" b="<<nodeRGB[2]<<endl;
 	}
-	*/
+	
 
 
 return EXIT_SUCCESS; 
