daee612c5c6b9d777efb0e79b2cb07480e5ac3e1
= <fbl773@mail.usask.ca>
2017-11-02 21:02:34 -0600

    So I've added directories. within them are the .cc and .h files we love, but we will need to make a makefile to compile them. with any luck, this will not render everythin useless.

---
+++ ledNode/ledNode.h
@@ -0,0 +1,63 @@
+//   Name: Ben Lewis
+//   Date: Oct 29th, 2017
+//   Synopsis: Arduino watch playground.
+
+//#include <iostream>
+#include <ctime>
+#include "arrayTools.h"
+
+using namespace std ; 
+
+//LedNodeStruct
+struct ledNode{
+
+//Colours
+int r, g, b;
+
+
+};
+
+//constructor
+/*
+* newLedNode()
+* - the struct equivilent of a constructor
+*
+* precond: none
+* postcond: new node declared and instantiated.
+* return: a pointer to the new node.
+*/
+ledNode* newLedNode();
+
+//ledNode setters
+/*
+* setNodeColour(r,g,b,n)
+* - sets the colour of node n to the int r g b value paramaters.
+* 
+* preconditions:
+* - n is not NULL
+*
+* postconditions:
+* - the r g b attributes of n are changed
+* return: nothing
+*/
+void setNodeColour(int,int,int,ledNode*);
+
+void setBlueSecond(ledNode*,int);
+
+void setGreenMinute(ledNode*,int);
+
+void setAverageCross(ledNode*,ledNode*);
+
+
+//Utilities
+/*
+* nodeStats(n)
+* prints the stats of ledNode* n
+* precond: n != NULL
+* postcond: stats desplayed
+* return: nothing 
+*/
+int* nodeStats(ledNode*);
+
+ledNode* genNodeArray(tm*);
+
