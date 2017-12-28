c88faf05c8e580c8932a8ac8774ed050d84da403
Bennett Lewis <bennettwlewis@hotmail.com>
2017-12-28 16:05:36 -0700

    so the watch is behaving just about exactly how I'd like it to. I like the colours I picked, the on/off is working nicely, the flourish is working nicely. Dare i say it's perfect? No. While it is indeed a masterpiece I have written we still need to comment it (because I got lazy) as well as check for possible performance hinderences. The clearStrip() function I wrote has been a god send but it does not get along well with colourWipe. Quickly realizing that the toughest part of building this watch will probably be the construction of the physical watch case. not writing the code.

--- Arduino/flowTests/flowTests.ino
+++ Arduino/flowTests/flowTests.ino
@@ -8,9 +8,7 @@
 #endif
 
 #define PIN 6
-
 #define NUM_LEDS 12
-
 #define BRIGHTNESS 20
 
 //For Time
@@ -18,10 +16,15 @@
 #define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
 
 //For Flow
-const uint8_t buttonPin = 8;     // the number of the pushbutton pin
-//const uint8_t ledPin =  6;      // the number of the LED pin
-// variables will change:
-uint8_t buttonState = 0;         // variable for reading the pushbutton status
+
+//ShowTimeButton
+const uint8_t startWatchPin = 8;     // the number of the pushbutton pin INPUT
+//const uint8_t watchPin =  6;      // the number of the LED pin OUTPUT
+boolean on = false;         //current output state
+int buttonState = 0;
+
+//For Flourish
+bool flourish = true;
 
 //For Ring
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
@@ -32,39 +35,71 @@ uint32_t hrColour = strip.Color(255,100,0,5);
 uint32_t minColour = strip.Color(0,255,95,0);
 uint32_t secColour = strip.Color(0,160,255,0);
 uint32_t blank = strip.Color(0,0,0,0);
+
+//Preconditions
 void setup() {
 
-    // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
   #if defined (__AVR_ATtiny85__)
     if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
   #endif
   // End of trinket special code
+
+  //For LED ring
   strip.setBrightness(BRIGHTNESS);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-  
+
+  //For god knows what
   Serial.begin(9600);
-  //pinMode(13, OUTPUT);
   setSyncProvider(requestSync);  //set function to call when sync required
   //Serial.println("Waiting for sync message");
+
+  //For Buttons
+  pinMode(startWatchPin,INPUT);
+ // pinMode(watchPin, OUTPUT);
   
   //Test Time for testing.
-  setTime(3,14,45,12,28,2017);
+  setTime(1,14,45,12,28,2017);
 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //Flourish
-  learnOffsets(200);
-
-  //TrackTime
-  while(true){
-    time_t t=now();
-    trackTime(t);
-  }
+  time_t t =now();
   
+
+  buttonState=digitalRead(startWatchPin);
+
+  if(buttonState == HIGH){
+     if(on == true)
+       on = false;
+     else
+       on = true;
+  }
+
+  if(on == true){
+      t=now();
+      
+      if(flourish){
+         modMinColour(t);
+         colorWipe(minColour,400);
+         clearStrip();
+         strip.show();
+         delay(700);
+         flourish = false;
+      }
+      trackTime(t);
+     
+    }
+   else{
+    clearStrip();
+    strip.show();
+    flourish = true;
+   }
+
+    delay(200);
   
 }
 
@@ -75,7 +110,7 @@ time_t requestSync(){
 }
 
 
-//My Functions
+//****************FUNCTIONS*********************
 
 //Time Index Functions
 uint8_t getHourIndex(time_t localTime){
@@ -108,7 +143,7 @@ uint32_t getAverageCross(uint32_t colorA, uint32_t colorB){
   return((colorA+colorB)/2);
 }
 
-void modMinuteRGB(time_t localTime){
+void modMinColour(time_t localTime){
     //modify Minutes colour
   uint32_t minMod = ((minute(localTime)%5)*64);
   if(minMod > 0)
@@ -119,12 +154,11 @@ void modMinuteRGB(time_t localTime){
   return;
 }
 
-void learnOffsets(uint16_t wait){
-  for(uint8_t i = 0; i<strip.numPixels();i++){
-     clearStrip();
-     strip.setPixelColor(i,strip.Color(15,(i*5),150,50));
-     delay(wait);
-     strip.show();
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint8_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);    
+    strip.show();
+    delay(wait);
   }
 }
 
@@ -134,27 +168,22 @@ void trackTime(time_t localTime){
   uint8_t minIdx = getMinuteIndex(localTime);
   uint8_t secIdx = getSecondIndex(localTime);
  
-  modMinuteRGB(localTime); 
+  modMinColour(localTime); 
   //No Overlap regualr 
   strip.setPixelColor(hrIdx,hrColour);
   strip.setPixelColor(minIdx,minColour);
   strip.setPixelColor(secIdx,secColour);
  
-  // If Hands Cross, average them.
-  if (secIdx == minIdx && minIdx == hrIdx){
+  // If Hands Cross, Their indicies will be the same so I average their set colours.
+  if (secIdx == minIdx && minIdx == hrIdx)
     strip.setPixelColor(minIdx,getAverageCross((getAverageCross(secColour,minColour)),hrColour));
-  }
-  else if(secIdx == minIdx){
+  else if(secIdx == minIdx)
     strip.setPixelColor(minIdx,getAverageCross(secColour,minColour));
-  }
-  else if(secIdx == hrIdx){
+  else if(secIdx == hrIdx)
     strip.setPixelColor(hrIdx,getAverageCross(secColour,hrColour));
-  }
-  else if(hrIdx == minIdx){
+  else if(hrIdx == minIdx)
     strip.setPixelColor(hrIdx, getAverageCross(hrColour,minColour));
-    //strip.setPixelColor(minIdx, getAverageCross(minColour,hrColour)); //Should only need one but things are goofy.
-  }
-
+  
   strip.show();
   clearStrip();
 }
