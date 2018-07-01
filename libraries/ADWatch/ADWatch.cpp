78e57d7ed36bb84bb09123391fdebfd4af289f3e
fbl773 <bennettwlewis@hotmail.com>
2018-07-01 12:43:52 -0600

    Updated the shit out of the comments on the main class. oooooh baby that uniformity is ballin'

--- libraries/ADWatch/ADWatch.cpp
+++ libraries/ADWatch/ADWatch.cpp
@@ -1,6 +1,6 @@
 /*
 Author: Ben Lewis
-Date: May 10th
+Date: May 10th, 2018
 
 Synopsis: 
 */
@@ -8,27 +8,15 @@ Synopsis:
 #include "ADWatch.h"
 
 
+/* ADWatch
+	- the implementation for ADWatch.h
+*/
 
-//****************************************************************************************
-//AD WATCH//
-//****************************************************************************************
-//Initializers
-/*
- *ADWatch()
- *
- *	precond: timeT is a valid time and Neopixel is instantiated
- * 	postcond: A new ADWatch object is created
- *
- *	Paramaters: 
- *
- *	Synopsis:
- */
 //ADWatch::ADWatch(Adafruit_GPS g, Adafruit_NeoPixel neoP){} //For when GPS is incorperated
 
 ADWatch::ADWatch(time_t trackMe, Adafruit_NeoPixel strip){
 	strip.setBrightness(20);
 	ring = strip;
-	//ring.setBrightness(20);
 	//clock = new Clock(trackMe,strip);
 	//compass = new Compass(0,strip);
 	//speedo = new Speedometer(0,strip);
@@ -38,75 +26,115 @@ ADWatch::ADWatch(time_t trackMe, Adafruit_NeoPixel strip){
 
 //Views
 //TODO test
+/* showTime()
+	precond: t is a valid time_t object
+	postcond: time is desplayed on the ring
+
+	paramaters: t - the time_t object who's time we will display
+	
+	synopsis: Takes in a time_t object, passes it to the trackTime function within the
+			  Clock module where it is decomposed between Gears and Face modules to produce
+			  A visual representation of the time!
+
+	return: nothing
+*/
 void ADWatch::showTime(time_t t){
 	clock->trackTime(t); 
 }
 
 //TODO test
+/* showSpeed()
+	precond: float s is a float in range[0.0,**200.0] **double check range
+	postcond: speed is displayed on the ring indexing fomr 6 up by 20km/h
+	
+	parameters: s - the speed we will display
+	
+	synopsis: takes in an initial speed to track (in knots from gps), and displays
+			  its value in kmph on the ring.
+
+	return: nothing.
+*/
 void ADWatch::showSpeed(float s){
 	speedo->setSpeed(s);
 	speedo->trackSpeed(s);
 }
 
 //TODO test
+/* showHeading()
+	precond: h is a float in range [0.0,360.0)
+	postcond: current heading is displayed using 30-degree intervals.
+	
+	parameters: h - the float heading we will track 
+
+	synopsis: h is passed to the trackHeading method of the Compass Module where it is 
+			  decomposed between the Needle and Magnet modules which, using 12:00, index 0, 
+			  to represent North (0-degrees), and a seperate light to show heading based on 
+			  every 30 degree change.
+
+	return: nothing
+*/
 void ADWatch::showHeading(float h){
 	compass->trackHeading(h);
 }
 
+//TODO test
+/* showLight()
+	precond: none
+	postcond: the light of Gandalf's staff fills the watch with ancient power and causes 
+			  illumination.
+
+	paramaters: none
+	
+	synopsis: turns all the lights to white and On...
+
+	return: nothing
+*/
 void ADWatch::showLight(void){
 	light->on();
 }
 
-void ADWatch::showStrobe(uint8_t stay){
+//TODO test
+/* showStrobe()
+	precond: none
+	postcond: party gets statrted
+	
+	paramters: ceasePin - the pin that dictates the starting and stopping of the strobe
+	
+	synopsis: gets the party started, strobes at a rate of <idk> /s in white (for now)
+*/
+void ADWatch::showStrobe(uint8_t ceasePin){
 	delay(45);
 	light->strobe(stay);
 }
 	
 
-//Internal Features
-//TODO implement
-void ADWatch::flashlight(void){
-	//Define Colour
+//Flow tools
+/* setPixels()
+	precond: c is a valid colour code
+	postcond: all pixels are *SET* to a the given colour.
 
-	uint32_t white = ring.Color(0,0,0,255);
-	//Set Pixels
-	flourish(white,100);	
-	
-	//Show Light!
-	ring.show();
+	parameters: c - the uint32 that represents the colour to be displayed.
 
-	return;
-}
+	synopsis: All pixels are set to colour c via a for loop.
 
-void ADWatch::strobe(bool stay){
-	//Define Colour
-	uint32_t white = ring.Color(0,0,0,255);
-
-	//Strobe
-	while(stay){
-		//wait
-		delay(30);
-
-		//On
-		ring.clear();
-		ring.show();
-		
-		//wait		
-		delay(100);
-		
-		//off
-		setPixels(white);
-		ring.show();
-	}
-	return;
-}
-
+	return: nothing
+*/
 void ADWatch::setPixels(uint32_t c){	
 	for(uint8_t i =0; i<12; i++)
 		ring.setPixelColor(i,c);		
 }
 
-//Flow tools
+/* flourish()
+	precond: colour is valid and wait isn't really long
+	postcond: the provided colour is spun around the indicies.
+
+	parameters: colour - the uint32 that will be flourished about the ring
+				wait - the uint32 that determines rate of flouish
+
+	synopsis: produces a stream-like effect passing the given colour around the indicies.
+
+	return: nothing
+*/
 void ADWatch::flourish(uint32_t colour, uint32_t wait){
 		for(uint8_t i=0; i <12;i++){
 			ring.setPixelColor(i,colour);
@@ -117,8 +145,4 @@ void ADWatch::flourish(uint32_t colour, uint32_t wait){
 		ring.show();
 }
 
-void ADWatch::clearRing(void){
-	ring.clear();
-//	ring.show();
-}
 
--- libraries/ADWatch/ADWatch.h
+++ libraries/ADWatch/ADWatch.h
@@ -12,6 +12,7 @@
 
 /* ADWatch
    -----------
+	-The union of all feature classes.
 	
 */
 class ADWatch{
@@ -22,37 +23,37 @@ class ADWatch{
 		//Implements
 		Adafruit_NeoPixel ring;
 		//Adafruit_GPS gps;	
+		
+		//Features
 		Clock* clock;
 		Compass* compass;
 		Speedometer* speedo;
 		Flashlight* light;
 
-		//****Methods*****/
+		//Methods
+
 		//Initializers
 		//ADWatch(Adafuit_GPS, Adafruit_NeoPixel); //For when GPS is incorperated
 		ADWatch(time_t, Adafruit_NeoPixel);
 		
-		//Views
+		//Views (Features)
 		void showTime(time_t);//Can probably be void when GPS gets here!
 		void showSpeed(float);
 		void showHeading(float);
 		void showLight(void);
 		void showStrobe(uint8_t);
-	
-		//Internal Features
-		void flashlight(void);
-		void strobe(bool);
-		void setPixels(uint32_t);
 
+		void setPixels(uint32_t);
 		//Flow tools
 		void flourish(uint32_t,uint32_t);
 		void clearRing(void);
-		
-	private:
-		//Features
-	//	Clock* clock;
-	//	Speedometer* speedo;
-	//	Compass* compass;
+	
+	/*
+		In the future, it may be a good refactor to make all of our features private and store
+		the colours we need to pull from them in an internal array or something. If we need 	
+		methods (which we shoudln't) we can just keep them public.
+	*/
+	//private:
 
 };
 
