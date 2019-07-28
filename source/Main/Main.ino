3c38e591431245e10e4458975a67c1acbd1e4d4c
fbl773 <fbl773@mail.usask.ca>
2019-07-28 15:39:47 -0600

    Setup sym links and main ino

--- source/Main/Main.ino
+++ source/Main/Main.ino
@@ -1,9 +1,272 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+#include <Adafruit_NeoPixel.h>
+#include <ADWatch.h>
+#include <TimeLib.h>
+#include <GPSTools.h>
 
+/**
+ *  WATCH SETUP BEGIN
+ */
+
+#define PIN 6
+#define NUM_LEDS 12
+#define BRIGHTNESS 30
+
+
+//For Ring
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel* ring = &strip;
+
+
+
+//Using a master watch Class
+ADWatch watch = ADWatch(ring);
+
+//For Flow
+//ShowTimeButton
+const uint8_t startWatchPin = 8;// the number of the pushbutton pin INPUT
+boolean on = false;         //current output state
+int buttonState = 0;       //the current flow through the button.
+bool isRunning = false;     //whether or not to do the light show on button press
+
+//Flourish Colours
+uint32_t compassColour;
+uint32_t speedoColour;
+uint32_t clockColour;
+uint32_t flashColour;
+uint32_t partyColour;
+uint32_t errorColour;
+uint32_t blank;
+uint32_t  err = ring->Color(255,0,0,0);
+
+enum Feats {Clock=0,Compass,Speedometer,Flashlight,Strobe,Refresh,Blank,FeatCount = 6}; // Remember to update count if features are added
+uint32_t Colours[FeatCount];
+int curFeat;
+
+/** END WATCH SETUP */
+
+
+// If you're using a GPS module:
+// Connect the GPS Power pin to 5V
+// Connect the GPS Ground pin to ground
+// If using software serial (sketch example default):
+//   Connect the GPS TX (transmit) pin to Digital 3
+//   Connect the GPS RX (receive) pin to Digital 2
+// If using hardware serial (e.g. Arduino Mega):
+//   Connect the GPS TX (transmit) pin to Arduino RX1, RX2 or RX3
+//   Connect the GPS RX (receive) pin to matching TX1, TX2 or TX3
+
+// If you're using the Adafruit GPS shield, change
+// SoftwareSerial mySerial(3, 2); -> SoftwareSerial mySerial(8, 7);
+// and make sure the switch is set to SoftSerial
+
+// If using software serial, keep this line enabled
+// (you can change the pin numbers to match your wiring):
+SoftwareSerial mySerial(3, 2);
+
+// If using hardware serial (e.g. Arduino Mega), comment out the
+// above SoftwareSerial line, and enable this line instead
+// (you can change the Serial number to match your wiring):
+
+//HardwareSerial mySerial = Serial1;
+
+
+Adafruit_GPS GPS(&mySerial);
+GPSTools* gTools;
+#define GPSECHO  false
+
+// this keeps track of whether we're using the interrupt
+// off by default!
+boolean usingInterrupt = false;
+void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
+
+/*Direct-GPS Setup*/
+time_t trackMe;
+
+void setup()
+{
+
+    /** GPS SETUP*/
+    gTools->gpsSetup();
+    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+    useInterrupt(true);
+    gTools = new GPSTools(&GPS);
+    gTools->gps->begin(9600);
+    setSyncProvider(gTools->grabTime());
+    setSyncInterval(10);
+    //trackMe = now();
+    /**
+     * WATCH SETUP
+     */
+//INIT RING
+    ring->setBrightness(BRIGHTNESS);
+    ring->begin();
+    ring->clear();
+    ring->show(); //Supposedly initilizes all to off
+
+    //For Buttons
+    pinMode(startWatchPin,INPUT);
+
+    //Test values
+    //setTime(1,24,30,12,28,2017);
+
+    //Init Flourish Colours & Cycler
+    clockColour = watch.clock_colour;
+    compassColour = watch.compass_colour;
+    speedoColour = watch.speedo_colour;
+    flashColour = watch.light_colour;
+    partyColour = watch.party_colour;
+    errorColour = watch.error_colour;
+    blank = watch.blank;
+
+    Colours[Clock] =clockColour;
+    Colours[Compass] = compassColour;
+    Colours[Speedometer] = speedoColour;
+    Colours[Flashlight] = flashColour;
+    Colours[Strobe] = partyColour;
+    Colours[Refresh] = errorColour;
+    Colours[Blank] = blank;
+
+    curFeat =0;
+
+    /**END WATCH SETUP*/
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+        char c = GPS.read();
+        // if you want to debug, this is a good time to do it!
+#ifdef UDR0
+if (GPSECHO)
+    if (c) UDR0 = c;
+    // writing direct to UDR0 is much much faster than Serial.print
+    // but only one character can be written at a time.
+#endif
+}
 
+void useInterrupt(boolean v) {
+    if (v) {
+        // Timer0 is already used for millis() - we'll just interrupt somewhere
+        // in the middle and call the "Compare A" function above
+        OCR0A = 0xAF;
+        TIMSK0 |= _BV(OCIE0A);
+        usingInterrupt = true;
+    } else {
+        // do not call the interrupt function COMPA anymore
+        TIMSK0 &= ~_BV(OCIE0A);
+        usingInterrupt = false;
+    }
+}
+
+void loop()                     // run over and over again
+{
+    //trackMe = now();
+    if (! usingInterrupt) {
+        char c = GPS.read();
+
+        if (GPSECHO)
+            if (c) Serial.print(c);
+    }
+
+    if (GPS.newNMEAreceived()) {
+        if (!GPS.parse(GPS.lastNMEA()))
+            return;
+    }
+
+    /** WATCH LOOP*/
+    buttonState=digitalRead(startWatchPin);
+    if(buttonState == HIGH){
+        if(on == true)
+            on = false;
+        else
+            on = true;
+    }
+
+    //Start watch button code.
+    if(on == true){
+        if(isRunning == false){
+            watch.flourish(Colours[curFeat],100);
+            delay(800);
+
+            if(curFeat == Clock){
+                //time_t gpsTime = gTools->grabTime();
+                //setTime(gTools->gps->hour,gTools->gps->minute,gTools->gps->seconds,gTools->gps->day,
+                //gTools->gps->month,gTools->gps->year);
+                //setTime(1,30,45,12,28,2017);
+                //setTime(gpsTime);
+            }
+
+
+            isRunning = true;          //5. remember not to florish every time we show the time
+        }
+        delay(200);
+        watch.setPixels(blank);
+        ring->show();
+        switch (curFeat) {
+            case Clock:
+                trackMe = now();
+                watch.showTime(trackMe);
+//                Serial.println("gTools");
+//                Serial.print(gTools->gps->hour, DEC); Serial.print(':');
+//                Serial.print(gTools->gps->minute, DEC); Serial.print(':');
+//                Serial.print(gTools->gps->seconds, DEC); Serial.print('.');
+//                Serial.println("GPS");
+//                Serial.print(GPS.hour, DEC); Serial.print(':');
+//                Serial.print(GPS.minute, DEC); Serial.print(':');
+//                Serial.print(GPS.seconds, DEC); Serial.print('.');
+                //setFlag(3);
+                break;
+            case Compass:
+                //setFlag(1);
+                if(gTools->hasFix())
+                    watch.showHeading(gTools->grabHeading());
+                else
+                    watch.refresh(!gTools->hasFix());
+                break;
+            case Speedometer:
+                //setFlag(2);
+                if(gTools->hasFix())
+                    watch.showSpeed(gTools->grabSpeed());
+                else
+                    watch.refresh(!gTools->hasFix());
+                break;
+            case Flashlight:
+                //setFlag(3);
+                watch.showLight();
+                break;
+            case Strobe:
+                //setFlag(4);
+                watch.showStrobe(startWatchPin);
+                break;
+            case Refresh:
+                watch.refresh(!gTools->hasFix());
+                break;
+            default:
+                watch.showError(errorColour);
+                break;
+        }
+
+        if(digitalRead(startWatchPin) == HIGH)
+            curFeat++;
+
+        if(curFeat > FeatCount-1)
+            curFeat = 0;
+
+
+        ring->show();
+        /**********************************/
+        delay(500);
+
+    }
+    else{
+        ring->clear();             //1. Button must be off, clear the strip
+        ring->show();
+        isRunning = false;                //3. remember to flourish when we turn it back on.
+    }
+
+
+    delay(200);                     //Apparently good for 'debounce' whatever that is
+    /*END WATCH LOOP*/
 }
