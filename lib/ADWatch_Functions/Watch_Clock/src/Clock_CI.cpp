7c532ec3ba693799684e9288d895b6b336979190
fbl773 <fbl773@mail.usask.ca>
2020-02-23 22:34:46 -0600

    Wrote Unit Tests For Gears

--- lib/ADWatch_Functions/Watch_Clock/src/Clock_CI.cpp
+++ lib/ADWatch_Functions/Watch_Clock/src/Clock_CI.cpp
@@ -7,7 +7,6 @@ build, I've created this class. Perhaps I will add some tests in here too.. even
 */
 
 #include "Clock_CI.h"
-#include <iostream>
 
 using namespace std;
 
@@ -16,35 +15,225 @@ using namespace std;
 //Clock_CI
 //****************************************************************************************
 
+/* CLOCK_GEARS TESTS */
+
+/* TEST HOLDERS */
+
+/** gears_Constructer*/
+void gears_Constructor(){
+	cout<<"Testing Gears Constructor"<<endl;
+
+	//Setup
+	int tstHr = 12;
+	int tstMin = 1;
+	int tstSec = 36;
+	setTime(tstHr,tstMin,tstSec);
+
+	//Construction
+	Clock_Gears* testGears = new Clock_Gears();
+	time_t result = testGears->getCurTime();
+
+	//Asserts
+	assert(testGears != NULL);
+	assert(hour(result) == tstHr);
+	assert(minute(result) == tstMin);
+	assert(second(result) == tstSec);
+	
+	cout<<"Gears Construction -- PASSED"<<endl;
+}
 
 
+void gears_UpdateTime(){
+	cout<<"Testing Gears Update Time"<<endl;
+	
+	//Setup
+	int tstHr = 12;
+	int tstMin = 1;
+	int tstSec = 36;
+	setTime(tstHr,tstMin,tstSec);
+	Clock_Gears* testGears = new Clock_Gears();
+	time_t tstTime = 15;
+	
+	//Testing
+	testGears->updateTime(tstTime);
+	time_t result = testGears -> getCurTime();
+	
+	assert(result == tstTime);
+	cout<<"Gears Update Time -- PASSED"<<endl;
+}
+
+/** Tests for Index Retrival*/
+void gears_GetIndex(){
+	cout<<"Testing Gears Get Index"<<endl;
+	Clock_Gears* testGears = new Clock_Gears();
+
+	cout<<"Testing Hour Index Retrival..."<<endl;
+	for(uint8_t i =0; i < 24; i++){
+		//Setup
+		setTime(i,0,0);
+		testGears->updateTime(now());
+		
+		//Testing
+		uint8_t result = testGears->getHourIndex();
+
+		//Result should never be greater than 11.
+		assert(result <= 11);
+
+		//Assets
+		if( i <= 11 ){
+			assert(i == result);
+		} else {
+			//24 hour time
+			uint8_t modded = i - 12;
+			assert(modded == result);
+		}
+	}
+	
+	cout<<"Gears Hour Index Retrival -- PASSED"<<endl;
 
 
+	cout<<"Testing Minute Index Retrival..."<<endl;
+	for(uint8_t i=0; i < 60; i++){
+		//Setup
+		setTime(0,i,0);
+		testGears->updateTime(now());
+		
+		//Testing
+		uint8_t result = testGears->getMinuteIndex();
+
+		//Result should never be greater than 11
+		assert(result <= 11);
+
+		// 0
+		if( i >= 0 && i < 5){
+			assert(result == 0);
+		// 5
+		} else if ( i >= 5 && i < 10){
+			assert(result == 1);
+		// 10 
+		} else if ( i >= 10 && i < 15){
+			assert(result == 2);
+		// 15 
+		} else if ( i >= 15 && i < 20){
+			assert(result == 3);
+		// 20
+		} else if ( i >= 20 && i < 25){
+			assert(result == 4);
+		// 25
+		} else if ( i >= 25 && i < 30){
+			assert(result == 5);
+		// 30
+		} else if ( i >= 30 && i < 35){
+			assert(result == 6);
+		// 35
+		} else if ( i >= 35 && i < 40){
+			assert(result == 7);
+		// 40
+		} else if ( i >= 40 && i < 45){
+			assert(result == 8);
+		//45
+		} else if ( i >= 45 && i < 50){
+			assert(result == 9);
+		//50
+		} else if ( i >= 50 && i < 55){
+			assert(result == 10);
+		//55
+		} else if ( i >= 55 && i < 60){
+			assert(result == 11 );
+		//60
+		} else if ( i >= 60 ){
+			assert(result == 0 );
+		}
+	}
+	
+	cout<<"Gears Minute Index Retrival -- PASSED"<<endl;
+		
+
+	cout<<"Testing Second Index Retrival..."<<endl;
+	for(uint8_t i = 0; i < 60; i++){
+		//Setup
+		setTime(0,0,i);
+		testGears->updateTime(now());
+		
+		//Testing
+		uint8_t result = testGears->getSecondIndex();
+
+		//Result should never be greater than 11
+		assert(result <= 11);
+
+		// 0
+		if( i >= 0 && i < 5){
+			assert(result == 0);
+		// 5
+		} else if ( i >= 5 && i < 10){
+			assert(result == 1);
+		// 10 
+		} else if ( i >= 10 && i < 15){
+			assert(result == 2);
+		// 15 
+		} else if ( i >= 15 && i < 20){
+			assert(result == 3);
+		// 20
+		} else if ( i >= 20 && i < 25){
+			assert(result == 4);
+		// 25
+		} else if ( i >= 25 && i < 30){
+			assert(result == 5);
+		// 30
+		} else if ( i >= 30 && i < 35){
+			assert(result == 6);
+		// 35
+		} else if ( i >= 35 && i < 40){
+			assert(result == 7);
+		// 40
+		} else if ( i >= 40 && i < 45){
+			assert(result == 8);
+		//45
+		} else if ( i >= 45 && i < 50){
+			assert(result == 9);
+		//50
+		} else if ( i >= 50 && i < 55){
+			assert(result == 10);
+		//55
+		} else if ( i >= 55 && i < 60){
+			assert(result == 11 );
+		//60
+		} else if ( i >= 60 ){
+			assert(result == 0 );
+		}
+	}
+	cout<<"Gears Seconds Index Retrival -- PASSED"<<endl;
+	return;
+
+}
 
 
+int Clock_GearsTests(){
+	int clock_pass = 1;
+
+	cout<<"****************** TESTING GEARS ****************************"<<endl;
+	gears_Constructor();
+	gears_GetIndex();
+
+	return 0;
+}
 
 
+int Clock_FaceTests(){
+	return 0;
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int ClockTests(){
+	return 0;
+}
 
 
 int main(){
 	int success = 1; // Exit of 1 assumes failure
 	cout<<"BEGINNING UNIT TESTS FOR CLOCK"<<endl;
+
+	Clock_GearsTests();
 	return 0;
 }
 
