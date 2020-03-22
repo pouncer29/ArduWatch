5542f6b50bda93294f38c20ca630dcd9a4aaf153
Bennett Lewis <bennettwlewis@hotmail.com>
2020-03-21 18:40:18 -0600

    Setup shell for Speedometer tests

--- lib/ADWatch_Functions/Watch_Speedometer/src/Speedometer_CI.cpp
+++ lib/ADWatch_Functions/Watch_Speedometer/src/Speedometer_CI.cpp
@@ -2,22 +2,52 @@
 Author: Ben Lewis
 Date: February 18th, 2020
 
-Synopsis: Because Arduino code is tough to make with gcc,and all I want is a verified
-build, I've created this class. Perhaps I will add some tests in here too.. eventually
+Synopsis: The tests for the speedometer
 */
 
 #include "Speedometer_CI.h"
+#include "Speed_Gauge.h"
+#include "Speed_Dial.h"
+#include <assert.h>
 #include <iostream>
 
 using namespace std;
 
-
-//****************************************************************************************
+//******************************************************************************
 //Speedometer_CI
-//****************************************************************************************
+//******************************************************************************
+
+/**
+	@Synopsis: Indexer Tests for the Speedometer (Gauge)
+*/
+int Gauge_Tests(){
+	cout<<"*********************** TESTING GAUGE ***********************"<<endl;
+	assert(false);
+	return 0;
+}
+
+/**
+	@Synopsis: Ring Interface tests for the Speedometer (Dial)
+*/
+int Dial_Tests(){
+	cout<<"*********************** TESTING Dial ************************"<<endl;
+	assert(false);
+	return 0;
+}
+
+/**
+	@Synopsis: Tests for the speedometer controller
+*/
+int Speedometer_Tests(){
+	assert(false);
+	return 0;
+}
 
 int main(){
-	cout<<"Welcome to the speedometer build..."<<endl;
+	cout<<"*********** BEGINNING UNIT TESTS FOR SPEEDOMETER ************"<<endl;
+	Gauge_Tests();
+	Dial_Tests();
+	Speedometer_Tests();
 	return 0;
 }
 
